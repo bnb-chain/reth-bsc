807ce3007c6f53d9ab18679cd2325e56b5266fcb
will-2012 <117156346+will-2012@users.noreply.github.com>
2025-09-02 09:29:48 +0800

    chore: polish trivals (#31)

--- src/node/evm/post_execution.rs
+++ src/node/evm/post_execution.rs
@@ -297,7 +297,6 @@ where
         if system_account.account.is_none() ||
             system_account.account.as_ref().unwrap().info.balance == U256::ZERO
         {
-            tracing::info!("System account is empty, block_number: {}, timestamp: {}", self.evm.block().number, self.evm.block().timestamp);
             return Ok(());
         }
 
